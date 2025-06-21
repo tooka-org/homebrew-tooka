fbec2d9478cc27ad5485482d77f460224d2b34d0
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2025-06-21 13:46:48 +0200

    Update tooka.rb

--- Formula/tooka.rb
+++ Formula/tooka.rb
@@ -4,12 +4,12 @@ class Tooka < Formula
   url "https://github.com/tooka-org/cli/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "6cba33272f49cc46f07247cb4d607a8dcbf415a0bc0aa16798c2faea2482d01e"
   license "GPL-3.0-only"
-  head "https://github.com/Benji377/tooka.git", branch: "main"
+  head "https://github.com/tooka-org/cli.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "tooka-cli")
+    system "cargo", "install"
   end
 
   test do
