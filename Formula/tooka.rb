c7a834c1e17fdbd562e787e09cabcd30cb95902e
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2025-06-28 16:17:03 +0200

    Update tooka.rb

--- Formula/tooka.rb
+++ Formula/tooka.rb
@@ -1,10 +1,10 @@
 class Tooka < Formula
   desc "Fast, rule-based CLI tool for organizing files"
   homepage "https://tooka.deno.dev"
-  url "https://github.com/tooka-org/cli/archive/refs/tags/v1.0.3.tar.gz"
+  url "https://github.com/tooka-org/tooka/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "d7ce3d212a8acc5bd706ac48dc60318cbf1ae7769dbc5603b70b12fc4f60ce82"
   license "GPL-3.0-only"
-  head "https://github.com/tooka-org/cli.git", branch: "main"
+  head "https://github.com/tooka-org/tooka.git", branch: "main"
 
   depends_on "rust" => :build
 
