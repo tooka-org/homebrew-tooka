c3da6c95c6a6164437e7618991a5ad1abaf88813
Benji377 <50681275+Benji377@users.noreply.github.com>
2025-07-03 03:52:26 +0000

    Bump Tooka formula to v1.0.5

--- Formula/tooka.rb
+++ Formula/tooka.rb
@@ -1,8 +1,8 @@
 class Tooka < Formula
   desc "Fast, rule-based CLI tool for organizing files"
   homepage "https://tooka.deno.dev"
-  url "https://github.com/tooka-org/tooka/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "6f412ff18076130e4facc1fef90c62e288d54c94554a249628a259e3b332c45b"
+  url "https://github.com/tooka-org/tooka/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "0582dce8a5f241f7cce78be04fcee51cc5e2c175df2ff524147018f755ccd1f6"
   license "GPL-3.0-only"
   head "https://github.com/tooka-org/tooka.git", branch: "main"
 
